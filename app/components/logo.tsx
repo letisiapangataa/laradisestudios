2098bd73bff1d5708c532a2a37b931cd3fabaedd
Letisia Pangata'a <51559861+letisiapangataa@users.noreply.github.com>
2024-04-09 15:25:03 +1200

    fixed responsive issues

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -5,16 +5,13 @@ import Particles from './particles';
 const Logo: React.FC = () => {
   return (
     <>
-    <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
     <Particles
-          className="grid inset-0 animate-fade-in width-200 height-200"
-          quantity={200} />
-          <h1 className="text-4xl text-transparent duration-900 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-              <img className="ls-width" src="https://res.cloudinary.com/louiy9obu/image/upload/v1712317603/LS_NoPreview_kvsyqs.png" alt="Laradise Studios Logo"></img>
-          </h1>
+          className="particles"
+          quantity={1300} />
+              <img className="ls-main-logo" src="https://res.cloudinary.com/louiy9obu/image/upload/v1712317603/LS_NoPreview_kvsyqs.png" alt="Laradise Studios Logo"></img>
           
           <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" /><div className="my-16 text-center animate-fade-in">
-              <h2 className="text-sm text-zinc-500 ">
+              <h2 className="ls-copyright">
                   © 2024 Laradise Studios
               </h2>
           </div>
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar: React.FC = () => {
     <>
 
 
-<svg className="hb" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" stroke="#FFF" stroke-width=".8" fill="rgba(0,0,0,0)" stroke-linecap="" style={{cursor: 'pointer'}} onClick={toggleSidebar}>
+<svg className="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" stroke="#FFF" stroke-width=".8" fill="rgba(0,0,0,0)" stroke-linecap="" style={{cursor: 'pointer'}} onClick={toggleSidebar}>
   <path d="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7">
     <animate dur="0.2s" attributeName="d" values="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7;M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7" fill="freeze" begin="start.begin" />
     <animate dur="0.2s" attributeName="d" values="M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7;M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7" fill="freeze" begin="reverse.begin" />
