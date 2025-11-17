2f6906a2c712210bfa968cc28b9e4f9a84609418
Letisia's Laboratory 🧪 <51559861+letisiapangataa@users.noreply.github.com>
2025-11-17 22:16:52 +1300

    made changes ready for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
 
         <div className="my-16 text-center animate-fade-in">
         <h2 className="ls-copyright">
-            © 2025 Laradise Studios. All Rights Reserved.
+            © 2025 Laradise Studios.
         </h2>
     </div>
     </>
