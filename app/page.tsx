4e87d2dcbf591a7621936793460272b31301b997
Letisia's Laboratory 🧪 <51559861+letisiapangataa@users.noreply.github.com>
2025-11-26 19:50:52 +1300

    added new files :)

--- app/page.tsx
+++ app/page.tsx
@@ -22,60 +22,31 @@ export default function Home() {
   
   return (
     <>
-      <div className={`sidebar fixed top-0 left-0 h-full w-64 z-50 transition-transform duration-300 ease-in-out transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-      {/* Sidebar content */}
-      <div className="p-8">
-        <p className="text-xl font-bold mt-4">
-            <Link href="/">
-            <img
-              className="ls-logo"
-              src="https://res.cloudinary.com/louiy9obu/image/upload/fl_preserve_transparency/v1712562804/LaradiseStudios_LOGO_2_kwdbcp.jpg?_s=public-apps"
-              alt="Laradise Studios Logo"
-              width="100%"
-            />
-          </Link>        
-        </p>
-        <li className="li-menu">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="li-menu">
-          <Link href="/ai-laradise-universe">
-            <strong>UNIVERSE x A.I.</strong>
-          </Link>
-        </li>
-        <li className="li-menu">
-          <Link href="/ai-music">
-            <strong>MUSIC x A.I.</strong>
-          </Link>
-        </li>
-           <li className="li-menu">
-          <Link href="/ai-artists">
-            <strong>ARTISTS x A.I.</strong>
-          </Link>
-        </li>
-        <li className="li-menu">
-          ☁️ 💗 ☁️
-        </li>
-        <li className="li-menu-sm">
-          <Link href="mailto:contact@laradisestudios.com">
-            Contact Us
-          </Link>
-        </li>
-        <SocialIcons />
-      </div>
-    </div>
-
     <div className="animate-fade-in">
-        <h2 className="ls-copyright">
-            Tap anywhere to get started.
-        </h2>
-     </div>
+  <h2 className="ls-copyright">
+    <a href="https://www.instagram.com/laradisestudios" target="_blank" rel="noopener noreferrer">
+      Instagram
+    </a>
+    {" x "}
+    <a href="https://www.linkedin.com/company/laradisestudios" target="_blank" rel="noopener noreferrer">
+      LinkedIn
+    </a>
+    {" x "}
+    <a href="https://www.tiktok.com/@laradisestudios" target="_blank" rel="noopener noreferrer">
+      TikTok
+    </a>
+    {" x "}
+    <a href="https://www.youtube.com/@laradisestudios" target="_blank" rel="noopener noreferrer">
+      YouTube
+    </a>
+  </h2>
+</div>
 
-    <Link href="" onClick={toggleSidebar}>
+
+
         <Particles
             className="particles"
             quantity={500} />
-    </Link>
 
      <Logo />
 
